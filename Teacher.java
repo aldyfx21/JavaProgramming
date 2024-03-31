11ef60c700cd4c6766010045c9e3168c5b428b85
aldyfx21 <165534294+aldyfx21@users.noreply.github.com>
2024-03-31 09:55:40 +0700

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,1 +1,32 @@
+class Teacher extends Person {
+    private int numCourses = 0;
+    private ArrayList<String> courses = new ArrayList<>();
 
+    public Teacher(String name, String address) {
+        super(name, address);
+    }
+
+    public boolean addCourse(String course) {
+        if (courses.contains(course)) {
+            return false;
+        } else {
+            courses.add(course);
+            numCourses++;
+            return true;
+        }
+    }
+
+    public boolean removeCourse(String course) {
+        if (!courses.contains(course)) {
+            return false;
+        } else {
+            courses.remove(course);
+            numCourses--;
+            return true;
+        }
+    }
+
+    public String toString() {
+        return "Teacher: " + super.toString();
+    }
+}
